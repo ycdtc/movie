e83fe636a650d8ae9263e35ba08fa60ff6b3df87
ycdtc <15210720204@fudan.edu.cn>
2017-06-21 18:38:24 +0800

    12123
    123123123

--- aaa/WebContent/javascript/base.js
+++ aaa/WebContent/javascript/base.js
@@ -47,3 +47,71 @@ function drawPie(chart,value,name){
 		ctx.fillText(name[i], 60 , 0);
 	}
 }
+
+function drawLine(chart,value,name,key){
+    var canvas = document.getElementById(chart);
+    var ctx = canvas.getContext('2d');
+    var cw = canvas.width;
+    var ch = canvas.height;
+    var padding = 80;
+    
+    var origin = {x:padding,y:ch-padding};
+    var bottomRight = {x:cw-padding,y:ch-padding};
+    var topLeft = {x:padding,y:padding};
+    //X
+    ctx.beginPath();
+    ctx.moveTo(origin.x,origin.y);
+    ctx.lineTo(bottomRight.x,bottomRight.y);
+    //Y
+    ctx.moveTo(origin.x,origin.y);
+    ctx.lineTo(topLeft.x,topLeft.y);
+
+    ctx.font = '16px SimHei';
+    
+    //绘制X方向刻度
+    var avgWidth = (cw - 2*padding - 50)/(key.length-1);
+    for(var i=0;i<key.length;i++){
+        if(i > 0){
+            ctx.moveTo(origin.x+i*avgWidth,origin.y);
+            ctx.lineTo(origin.x+i*avgWidth,origin.y-10);
+        }
+        var txtWidth = ctx.measureText(key[i]).width;
+        ctx.fillText(key[i],origin.x+i*avgWidth-txtWidth/2,origin.y+32);
+    }
+    //绘制Y方向刻度
+    var max = Math.max.apply(null,value.join(",").split(","));
+    var avgValue=Math.floor(max/5);
+    var avgHeight = (ch-padding*2-50)/5;
+    for(var i=0;i<key.length;i++){
+    	if(i>0){
+            ctx.moveTo(origin.x,origin.y-i*avgHeight);
+            ctx.lineTo(origin.x+10,origin.y-i*avgHeight);
+    	}
+        var txtWidth = ctx.measureText(avgValue*i).width;
+        ctx.fillText(avgValue*i,origin.x-txtWidth-5,origin.y-i*avgHeight+6);
+    }
+    ctx.stroke();
+    
+    //line
+    var labelX = padding;
+    for(var k=0;k<name.length;k++){
+    	ctx.beginPath();
+    	ctx.strokeStyle = colors[k];
+	    for(var i=0;i<value[k].length;i++){
+	        var posY = origin.y - value[k][i]/max*(ch-2*padding-50);
+	        ctx.fillStyle = colors[k];
+	        if(i==0){
+	            ctx.moveTo(origin.x+i*avgWidth,posY);
+	        }else{
+	            ctx.lineTo(origin.x+i*avgWidth,posY);
+	        }
+	    }
+	    ctx.moveTo(labelX,padding/2-6);
+        ctx.lineTo(labelX+80,padding/2-6);
+        ctx.fillStyle="black";
+        ctx.fillText(name[k],labelX+90,padding/2);
+        labelX += 100 + ctx.measureText(name[k]).width;
+        ctx.stroke();
+    }
+	
+}
